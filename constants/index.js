a008f6b53662d3b8557b5e0d2e7c8797d9164d63
marcrypto1 <107799758+marcrypto1@users.noreply.github.com>
2023-07-22 10:40:04 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xd8b5c9c228999c44c64057f4cb320dc746ac72dd";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
